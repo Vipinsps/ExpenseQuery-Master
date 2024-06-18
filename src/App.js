0958caa1eadee4ef4a026db7e310658858ce5f3f
suyabisht-sp <surya_bisht@softprodigy.com>
2024-06-18 15:17:09 +0530

    basic design added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Wave from './components/waves';
+
+function App() {
+  return (    
+    <div className="App">
+      <header className='header'>Welcome to EQM</header>
+      <Wave/>
+    </div>
+  );
+}
+
+export default App;
